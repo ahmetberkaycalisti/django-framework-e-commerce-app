6db9ef9c433032e5017d5319ef8bfe77a661770b
AHMET CALISTI <ahmetberkaycalisti@gmail.com>
2021-09-10 23:17:44 +0300

    seventh commit

--- carts/views.py
+++ carts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart,CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -12,7 +13,21 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+
     product = Product.objects.get(id=product_id)    # for get product id
+    product_variation = []
+    if request.method == "POST":
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+                print(variation)
+            except:
+                pass
+
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
@@ -56,6 +71,8 @@ def remove_cart_item(request,product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -64,7 +81,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         
         tax = (2 * total) / 100
         grand_total = total + tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass # just ignore it
 
     context = {
